1675e794cb03e67d0427c90d310d84ee2604b01a
Bleron <bleron3052@gmail.com>
2025-03-04 20:39:59 +0100

    Powered-By Header entfernt zur Verbesserung der Sicherheit, owaps kategorie 5

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const { initializeAPI } = require("./api");
 
 // Create the express server
 const app = express();
+
+app.disable("x-powered-by");
+
 app.use(express.json());
 const server = http.createServer(app);
 
